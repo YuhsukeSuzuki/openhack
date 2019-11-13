2e08b61fc5d4af118f87724aa9769b84e39f04fd
Akihiro Nishikawa <akihiro.nishikawa@oracle.com>
2019-11-13 15:07:35 +0900

    Validation logic is added.

--- src/main/java/com/bfyoc/team4/CreateRating.java
+++ src/main/java/com/bfyoc/team4/CreateRating.java
@@ -6,11 +6,14 @@ import com.microsoft.azure.functions.annotation.CosmosDBOutput;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
-
 
 /**
  * Azure Functions with HTTP Trigger.
@@ -22,28 +25,70 @@ public class CreateRating {
             @HttpTrigger(name = "req",
                     methods = {HttpMethod.POST},
                     authLevel = AuthorizationLevel.ANONYMOUS,
-                    route="rating") HttpRequestMessage<Optional<Rating>> request,
+                    route = "rating") HttpRequestMessage<Optional<Rating>> request,
             @CosmosDBOutput(name = "cosmosDB",
                     databaseName = "team4db",
                     collectionName = "team4container",
-                    connectionStringSetting="AzureCosmosDBConnection") OutputBinding<Rating> rating,
+                    connectionStringSetting = "AzureCosmosDBConnection") OutputBinding<Rating> rating,
             final ExecutionContext context) {
         context.getLogger().info("CreateRating function is invoked.");
 
         Rating _rating = request.getBody().get();
-        // guid
-        _rating.setId(UUID.randomUUID().toString());
-        // timestamp
-        ZonedDateTime zonedDateTime = ZonedDateTime.now();
-        DateTimeFormatter dtf = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-        _rating.setTimestamp(zonedDateTime.format(dtf));
+        String errorMessage = null;
+        try {
+            if ( invokeValidationAPI("https://serverlessohuser.trafficmanager.net",
+                    "/api/GetUser",
+                    "userId",
+                    _rating.getUserId())
+            && invokeValidationAPI("https://serverlessohproduct.trafficmanager.net",
+                    "/api/GetProduct",
+                    "productId",
+                    _rating.getProductId())
+            && (_rating.getRating() > -1 && _rating.getRating() < 6 )) {
 
-        // Insert data to Cosmos DB
-        rating.setValue(_rating);
+                // add elements to rating
+                // guid
+                _rating.setId(UUID.randomUUID().toString());
+                // timestamp
+                ZonedDateTime zonedDateTime = ZonedDateTime.now();
+                DateTimeFormatter dtf = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+                _rating.setTimestamp(zonedDateTime.format(dtf));
 
-        return request.createResponseBuilder(HttpStatus.CREATED)
-                .header("content-type","application/json")
-                .body(_rating)
-                .build();
+                // Insert data to Cosmos DB
+                rating.setValue(_rating);
+            }
+            else {
+                errorMessage = "Invalid attribute is found.";
+            }
+        } catch (Exception e) {
+            errorMessage = "Invalid attribute is found.";
+        }
+
+        if(Objects.isNull(errorMessage)) {
+            return request.createResponseBuilder(HttpStatus.CREATED)
+                    .header("content-type", "application/json")
+                    .body(_rating)
+                    .build();
+        }
+        else {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                    .header("content-type", "application/json")
+                    .body("{\"error\":\"" + errorMessage + "\"}")
+                    .build();
+        }
+    }
+
+    Boolean invokeValidationAPI(String _targetURI, String _path, String _queryParam, String queryValue) {
+        Response response = ClientBuilder.newClient()
+                .target(_targetURI)
+                .path(_path)
+                .queryParam(_queryParam, queryValue)
+                .request(MediaType.APPLICATION_JSON)
+                .get(Response.class);
+        if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
