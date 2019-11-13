709b1ab6757764a0170f3faedf7c6884bda5855a
Akihiro Nishikawa <akihiro.nishikawa@oracle.com>
2019-11-13 10:53:44 +0900

    CreateRating API

---
+++ src/main/java/com/bfyoc/team4/Rating.java
@@ -0,0 +1,67 @@
+package com.bfyoc.team4;
+
+public class Rating {
+    String  id;
+    String userId;
+    String productId;
+    String timestamp;
+    String locationName;
+    Integer rating;
+    String userNotes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getUserNotes() {
+        return userNotes;
+    }
+
+    public void setUserNotes(String userNotes) {
+        this.userNotes = userNotes;
+    }
+}
